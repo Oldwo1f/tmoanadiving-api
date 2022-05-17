86f6f663cde38f8ff8fa9514482dfc26e1e5d13a
Oldwo1f <alexismomcilovic@gmail.com>
2022-05-16 17:16:55 -1000

    hhh

--- config/datastores.js
+++ config/datastores.js
@@ -52,12 +52,11 @@ module.exports.datastores = {
     // url: 'mysql://user:password@host:port/database',
     host: 'localhost',
     port: 40061,
+    database: 'MADMIN',
     auth: {
       user: 'creapassionprod',
       password: 'KGZP2898_WrR',
     },
-    database: 'MADMIN'
-
   },
 
 
