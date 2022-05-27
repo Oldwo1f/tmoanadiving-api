c5ace94337778001fd5f585b1a7b9f70c19333e4
Oldwo1f <alexismomcilovic@gmail.com>
2022-05-27 10:20:54 -1000

    gre

--- config/datastores.js
+++ config/datastores.js
@@ -51,7 +51,7 @@ module.exports.datastores = {
     adapter: 'sails-mongo',
     // url: 'mysql://user:password@host:port/database',
     host: 'localhost',
-    port: 40063,
+    port: 40061,
     database: 'MADMIN',
     user: 'creapassionprod',
     password: 'KGZP2898_WrR',
