9e365c8cbc2995bc40be75a524478f0a2b78ebf6
Oldwo1f <alexismomcilovic@gmail.com>
2022-06-03 10:20:27 -1000

    rezrezrez

--- api/controllers/Jeu/tirage.js
+++ api/controllers/Jeu/tirage.js
@@ -50,13 +50,14 @@ module.exports = {
 		var random = Math.floor(Math.random() * inscrits.length)
 
 		var gagnant = inscrits.splice(random, 1)
-
+		console.log(gagnant);
 		const datas = {
-			winnerfirstname: gagnant[0].firstname,
-			winnerlastname: gagnant[0].lastname,
+			winnerfirstname: gagnant[0].firstName,
+			winnerlastname: gagnant[0].lastName,
 			winnercity: gagnant[0].city,
 			winneremail: gagnant[0].email,
 		}
+		console.log('datas', datas);
 		// var record = await Jeu.findOne(id).populate('images').populate('logos').populate('deinscrits').populate('inscrits').populate('imagesfin').populate('imagesgagnant')
 		var result = await Jeu.updateOne(id).set(datas)
 			.intercept({ name: 'UsageError' }, 'invalid');
